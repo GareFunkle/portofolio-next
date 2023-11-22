8f7e6d56619b4e04d6032c1000e0cb4c3caa033c
Vincent <vincent.durret@gmail.com>
2023-11-22 13:25:26 +0100

    //

--- src/components/Sections/SectionContact/SectionContact.js
+++ src/components/Sections/SectionContact/SectionContact.js
@@ -1,12 +1,38 @@
+import {
+  faGithub,
+  faLinkedin,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import React from "react";
 
 const SectionContact = () => {
   return (
-    <div id='contact' className='h-screen w-full '>
-      <div className='relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <h2 className='text-h1 font-bold text-white'>Contact</h2>
+    <section id='contact' className='h-[90vh] w-full '>
+      <div className='flex h-full w-full flex-col items-center justify-center'>
+        <h2 className='mb-4 text-h1 font-bold text-white'>Contact</h2>
+        <div className='mb-4 h-[1px] w-1/2 bg-gray-400'></div>
+        <p className='mb-4 text-center text-p  text-white'>
+          VOUS AVEZ UNE QUESTION, UNE PROPOSITION, UN PROJET OU VOUS VOULEZ
+          TRAVAILLER ENSEMBLE SUR QUELQUE CHOSE ? ENVOIE-MOI UN EMAIL
+        </p>
+        <div className='mb-4 h-[1px] w-1/2 bg-gray-400'></div>
+        <div className='flex flex-col items-center justify-center'>
+          <Link
+            href='https://www.linkedin.com/in/alexandre-roux-6b4b0b1b3/'
+            target='_blank'
+            rel='noreferrer'
+            className=''
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} className='w-12 text-white' />
+          </Link>
+          <Link href='#'>
+            <FontAwesomeIcon icon={faGithub} className='w-12 text-white' />
+          </Link>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
