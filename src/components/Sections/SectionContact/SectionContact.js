b6b727567a9127d70c233d7a477a9781e86b0864
Vincent <vincent.durret@gmail.com>
2023-11-23 11:02:52 +0100

    //

--- src/components/Sections/SectionContact/SectionContact.js
+++ src/components/Sections/SectionContact/SectionContact.js
@@ -14,20 +14,21 @@ const SectionContact = () => {
         <h2 className='mb-4 text-h1 font-bold text-white'>Contact</h2>
         <div className='mb-4 h-[1px] w-1/2 bg-gray-400'></div>
         <p className='mb-4 text-center text-p  text-white'>
-          VOUS AVEZ UNE QUESTION, UNE PROPOSITION, UN PROJET OU VOUS VOULEZ
-          TRAVAILLER ENSEMBLE SUR QUELQUE CHOSE ? ENVOIE-MOI UN EMAIL
+          Vous avez une question, une proposition, un projet ou vous voulez
+          travailler ensemble sur quelque chose ?
+          <a href='#'> Envoyer moi un email</a>
         </p>
         <div className='mb-4 h-[1px] w-1/2 bg-gray-400'></div>
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-row items-center justify-center'>
           <Link
             href='https://www.linkedin.com/in/alexandre-roux-6b4b0b1b3/'
             target='_blank'
             rel='noreferrer'
-            className=''
+            className='p-4'
           >
             <FontAwesomeIcon icon={faLinkedinIn} className='w-12 text-white' />
           </Link>
-          <Link href='#'>
+          <Link href='#' target='_blank' rel='noreferrer' className='p-4'>
             <FontAwesomeIcon icon={faGithub} className='w-12 text-white' />
           </Link>
         </div>
