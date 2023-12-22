556d3ab5be7077264de8693b866c2c588ca7f136
GareFunkle <vincent.durret@gmail.com>
2023-12-22 08:13:20 +0100

    add text for my projects

--- src/components/Sections/SectionExperience/SectionExperience.js
+++ src/components/Sections/SectionExperience/SectionExperience.js
@@ -44,12 +44,22 @@ const items = [
     src: "/images/portofolio/agence-digitale-community-management-Nice-antibes-cannes-univers-terrasses-bois.jpg",
     name: "Univers Terrasses Bois Stock",
     git: "https://github.com/Vincent-Durret/UTB-Stock.git",
+    goals:
+      "Création d'un application de gestion de stock en Vue.js avec Firebase pour l'entrprise Univers Terrasses Bois ",
+    dev: "Pour ce projet j'ai eu carte blanche pour le design et toute la partie technique ! cela ma pris 1 ans pour la finaliser mais j'ai pu y ajouter des fonctionalités tel que qu'un systeme de connexion pour les employer pour qu'il puisse inscrire la fourniture qu'il prenait dans le stock ou celle qui ramener du chantier pour la ranger ! Ce fut une experience enrichissante pour mon apprentissage.",
+    conclusion:
+      "Ce projet sur Vue.js a été une expérience enrichissante et un témoignage de ma capacité à guider les clients vers les meilleures solutions e-commerce. En partant d'un thème vierge, j'ai pu concevoir un site qui non seulement répond aux attentes spécifiques du client mais qui les dépasse, en intégrant des fonctionnalités innovantes et personnalisées. Chaque plugin développé, de la fonctionnalité de zoom sur les images des produits au carrousel interactif, a contribué à créer une interface utilisateur intuitive et une expérience d'achat attrayante. Ce succès souligne mon engagement envers l'excellence dans le développement web et ma passion pour la création de solutions e-commerce qui allient esthétique, fonctionnalité et performance. Ce projet est un exemple clé de mon expertise dans la conception de sites e-commerce qui non seulement attirent les clients, mais les fidélisent également.",
     imgSite: "/images/portofolio/website/utb-stock.png",
   },
   {
     categories: ["React/Next"],
     src: "/images/portofolio/logo_big_givi.png",
     name: "GIVI Agence Digitale",
+    goals:
+      "Création d'un site vitrine pour l'agence GIVI Agence Digitale avec Next.js",
+    dev: "Pour ce projet j'ai imaginer un site pour ma copine et moi dans le cadre de la communication digital. Elle ma fais toute la partie design que j'ai ensuite ecrit pour avoir le resultat attendu. J'ai reussi a comprendre le fonctionnement de Next.js avec ce projet et j'ai aussi ameliorer mon niveau de react grace a ce projet !",
+    conclusion:
+      "Le faite de pouvoir imaginer un site et de le voir prendre vie est une experience enrichissante pour moi !",
     git: "https://github.com/Vincent-Durret/givi-agence-digitale-next.git",
     imgSite: "/images/portofolio/website/givi-agence-digitale.png",
   },
@@ -57,6 +67,10 @@ const items = [
     categories: ["Python/Pygames"],
     src: "/images/portofolio/logo-traveler.png",
     name: "Traveler Game",
+    goals: "Création d'un jeu en python avec pygame",
+    dev: "J'ai créer ce jeu avec deux amis dont un qui etais graphiste et l'autre dans la communication. J'ai pu apprendre a utiliser pygame et a creer un jeu avec des fonctionalités basique mais qui fonctionne !",
+    conclusion:
+      "Ce fut un de mes premier projet de developpeur ce qui ma permit de comprendre le fonctionnement d'un projet en equipe et de pouvoir apprendre de mes erreurs !",
     git: "https://github.com/Vincent-Durret/work-traveler.git",
     imgSite: "/images/portofolio/website/traveler.png",
   },
@@ -79,14 +93,12 @@ const SectionExperience = () => {
     >
       <div className='container  mx-auto p-4'>
         <h1 className='text-h1 font-semibold text-white'>Experiences</h1>
-
         <div className='mt-8  lg:flex xl:mt-16'>
           <ExperienceLinks
             category={categories}
             onCategorySelect={setSelectedCategory}
             selectedCategory={selectedCategory}
           />
-
           <div className='mx-2 mt-8 flex-1 lg:mx-8 lg:mt-0'>
             <ExperienceItems
               items={items}
