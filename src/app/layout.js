34a8a23b678ef3248a5d15b2d640177ec6b0619a
GareFunkle <vincent.durret@gmail.com>
2024-01-04 16:45:38 +0100

    //

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Portofolio Vincent Durret",
+  title: "Vincent Durret Portfolio",
   description:
     "Découvrez le parcours unique d'un ancien électricien passionné par la technologie, désormais développeur web polyvalent. Spécialisé en Vue.js et en intégration web, je suis constamment à la recherche de nouveaux défis pour enrichir mes compétences et apporter des solutions innovantes. Explorez mon portfolio pour voir comment je peux transformer vos idées en réalité.",
 };
