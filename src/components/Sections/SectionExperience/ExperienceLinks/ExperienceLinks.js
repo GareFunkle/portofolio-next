7ea305c9b31e8c2ebb214bf8657c783b3893888e
GareFunkle <vincent.durret@gmail.com>
2024-01-04 16:21:21 +0100

    cahnge the name of ButtionLinkPortofolio

--- src/components/Sections/SectionExperience/ExperienceLinks/ExperienceLinks.js
+++ src/components/Sections/SectionExperience/ExperienceLinks/ExperienceLinks.js
@@ -1,12 +1,12 @@
-import ButtonPortofolio from "@/components/ui/buttonLinkPortofolio/buttonPortofolio";
 import React from "react";
+import ButtonLinkPortofolio from "@/components/ui/buttonLinkPortofolio/ButtonLinkPortofolio";
 
 const ExperienceLinks = (props) => {
   return (
     <div className='lg:mx-12'>
       <div className='mt-4 space-y-4  lg:mt-8'>
         {props.category.map((item, index) => (
-          <ButtonPortofolio
+          <ButtonLinkPortofolio
             isSelected={props.selectedCategory === item.name}
             onSelect={() => props.onCategorySelect(item.name)}
             key={index}
