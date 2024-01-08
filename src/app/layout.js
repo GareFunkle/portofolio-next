3b226a20cfa68a85c5958839b0b045db33c23807
GareFunkle <vincent.durret@gmail.com>
2024-01-08 17:18:46 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { Trispace } from "next/font/google";
+import Head from "next/head";
 
 const trispace = Trispace({
   subsets: ["latin"],
@@ -34,13 +35,39 @@ export const metadata = {
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='fr' className={trispace.className}>
-      <body>
-        {children}
-        <SpeedInsights />
-        <Analytics />
-      </body>
-    </html>
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name='description' content={metadata.description} />
+        <meta property='og:title' content={metadata.openGraph.title} />
+        <meta
+          property='og:description'
+          content={metadata.openGraph.description}
+        />
+        <meta property='og:url' content={metadata.openGraph.url} />
+        <meta property='og:site_name' content={metadata.openGraph.siteName} />
+        <meta property='og:locale' content={metadata.openGraph.locale} />
+        <meta property='og:type' content={metadata.openGraph.type} />
+        {metadata.openGraph.images.map((image, index) => (
+          <>
+            <meta property='og:image' content={image.url} />
+            <meta property='og:image:width' content={image.width.toString()} />
+            <meta
+              property='og:image:height'
+              content={image.height.toString()}
+            />
+            <meta property='og:image:alt' content={image.alt} />
+          </>
+        ))}
+      </Head>
+      <html lang='fr' className={trispace.className}>
+        <body>
+          {children}
+          <SpeedInsights />
+          <Analytics />
+        </body>
+      </html>
+    </>
   );
 };
 
